01fbff40095e902e1c57fea049d730966d079cf4
Lee Lawler <lawler_lee@icloud.com>
2020-01-12 19:36:41 -0600

    added random number generator to each gem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,12 @@ $(document).ready(function() {
 var ranNum = Math.floor(Math.random() * (120 - 19) + 19);
 //add random number within HTML
 $("#ranNum").text(ranNum);
+//give each gem img a random number
+var gem1 = Math.floor(Math.random() * 12 + 1);
+var gem2 = Math.floor(Math.random() * 12 + 1);
+var gem3 = Math.floor(Math.random() * 12 + 1);
+var gem4 = Math.floor(Math.random() * 12 + 1);
+
 
 
 
