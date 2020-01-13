a5dbfcd7ec73e238aa7e1e07bcbfa1b421046e86
Lee Lawler <lawler_lee@icloud.com>
2020-01-12 19:28:39 -0600

    updated README/added random number generator

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+// choose a random number between 19 and 120 at start of game
+var ranNum = Math.floor(Math.random() * (120 - 19) + 19);
+//add random number within HTML
+$("#ranNum").text(ranNum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
